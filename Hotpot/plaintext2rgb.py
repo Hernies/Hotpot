ea68c3243e9670cd5078468e1a552c7aa90b05ce
Hernies <hernancalvoaguiar.2@gmail.com>
2022-02-07 01:14:41 +0100

    first concept finalised, requires further tweaking

--- Hotpot/plaintext2rgb.py
+++ Hotpot/plaintext2rgb.py
@@ -3,24 +3,30 @@ from ctypes import sizeof
 from PIL import Image
 import math as m
 
+#TODO crear el concepto en una funcion tweakeable
+#TODO no usar una lista si no leer directamente del archivo
 
 def test():
-    f='test4.txt'
+    f='qran.txt'
     file = open(f)
     text = file.read()  
     file.close()            # this is dumb, should just read from file instead of dumping it into a 
     text = list(text)       #rudimentary fix, turn text into list so we can manage the characters
     size = m.floor(m.sqrt(len(text)//3))     #will only work for 5px images for now
     print(size)
-    img =  Image.new('RGB', (size,size), 'white')
+    # for elem in text:
+    #     print(ord(elem))
+
+    img =  Image.new('RGB', (size,size))
     pixels = img.load() # create the pixel map
     c = 0
     for i in range(img.size[0]):    # for every col:
         for j in range(img.size[1]):    # For every row
-            pixels[i,j] = (ord(text[c]),ord(text[c+1]) ,ord(text[c+2]) ) # set the colour accordingly
+            pixels[i,j] = (ord(text[c]), ord(text[c+1]), ord(text[c+2])) # set the colour accordingly
             c+=1
         c+=1    
     img.show()
+    img.save('qran1px1c.png')
         
 
 test()
